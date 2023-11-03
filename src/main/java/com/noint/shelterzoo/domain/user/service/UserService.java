7537e8aff9f4a04fc2ca427d42873024194050ad
no-intel <tkdduf212@gmail.com>
2023-11-03 19:47:44 +0900

    유저 서비스 익셉션 수정

--- src/main/java/com/noint/shelterzoo/domain/user/service/UserService.java
+++ src/main/java/com/noint/shelterzoo/domain/user/service/UserService.java
@@ -1,9 +1,11 @@
 package com.noint.shelterzoo.domain.user.service;
 
 import com.noint.shelterzoo.domain.user.dto.UserDTO;
+import com.noint.shelterzoo.domain.user.exception.UserException;
+import com.noint.shelterzoo.domain.user.enums.UserExceptionEnum;
 import com.noint.shelterzoo.domain.user.repository.UserRepository;
 import com.noint.shelterzoo.domain.user.vo.UserVO;
-import com.noint.shelterzoo.enums.UserStateEnum;
+import com.noint.shelterzoo.domain.user.enums.UserStateEnum;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -26,7 +28,7 @@ public class UserService {
     public void signup(UserDTO.Signup request) {
         String password = request.getPassword();
         if (!regexMatcher(PASSWORD_REG, password)){
-            throw new RuntimeException("패스워드 형식 불일치. 영문 + 숫자 + 최소 8글자 조합 필수");
+            throw new UserException(UserExceptionEnum.PASSWORD_INVALID);
         }
 
         request.setPassword(passwordEncoder.encode(password));
@@ -35,13 +37,13 @@ public class UserService {
             userRepository.signup(UserVO.Signup.create(request));
         } catch (DataIntegrityViolationException e) {
             String errorMsg = e.getMessage();
-            log.error("유저 회원가입 에러. param : " + errorMsg);
+            log.warn("유저 회원가입 에러. param : " + errorMsg);
             if (Objects.requireNonNull(errorMsg).contains("nickname")){
-                log.error("닉네임 중복 : " + request.getNickname());
-                throw new RuntimeException("닉네임 중복");
+                log.warn("닉네임 중복 : " + request.getNickname());
+                throw new UserException(UserExceptionEnum.NICKNAME_DUPLICATE);
             } else if (errorMsg.contains("email")) {
-                log.error("이메일 중복 : " + request.getEmail());
-                throw new RuntimeException("이메일 중복");
+                log.warn("이메일 중복 : " + request.getEmail());
+                throw new UserException(UserExceptionEnum.EMAIL_DUPLICATE);
             }
         }
     }
@@ -49,14 +51,14 @@ public class UserService {
     public Boolean isExistEmail(String email){
         int maxLength = 30;
         if (!regexMatcher(EMAIL_REGEX, email) || email.length() > maxLength){
-            throw new RuntimeException("이메일 형식 혹은 길이가 맞지 않습니다.");
+            throw new UserException(UserExceptionEnum.EMAIL_INVALID);
         }
         return userRepository.isExistEmail(email) >= 1;
     }
 
     public Boolean isExistNickname(String nickname){
         if (!regexMatcher(NICKNAME_REG, nickname)){
-            throw new RuntimeException("닉네임 형식 혹은 길이가 맞지 않습니다.");
+            throw new UserException(UserExceptionEnum.NICKNAME_INVALID);
         }
         return userRepository.isExistNickname(nickname) >= 1;
     }
@@ -69,19 +71,19 @@ public class UserService {
         String hashedPassword = userRepository.getPasswordByEmail(request.getEmail());
         if (!StringUtils.hasLength(hashedPassword)) {
             log.warn("로그인 실패 : '{}' 해당 이메일을 가진 유저가 존재 하지 않음", request.getEmail());
-            throw new RuntimeException("로그인 실패 : 이메일 또는 패스워드를 확인해 주세요");
+            throw new UserException(UserExceptionEnum.LOGIN_FAILD);
         }
 
         boolean matches = passwordEncoder.matches(request.getPassword(), hashedPassword);
         if (!matches){
             log.warn("로그인 실패 : 비밀번호 불일치");
-            throw new RuntimeException("로그인 실패 : 이메일 또는 패스워드를 확인해 주세요");
+            throw new UserException(UserExceptionEnum.LOGIN_FAILD);
         }
 
         UserVO.MyInfo myInfo = userRepository.myInfo(request.getEmail());
         if (!UserStateEnum.isStable(myInfo.getState())) {
             log.warn("로그인 실패 : 유저 가입상태 - {}", myInfo.getState());
-            throw new RuntimeException("로그인 실패 : 이메일 또는 패스워드를 확인해 주세요");
+            throw new UserException(UserExceptionEnum.LOGIN_FAILD);
         }
 
         return UserDTO.MyInfo.create(myInfo);
