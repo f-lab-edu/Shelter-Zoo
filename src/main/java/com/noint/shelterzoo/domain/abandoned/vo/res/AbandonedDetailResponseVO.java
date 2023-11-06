6f2d57597263842e3851b07cdf6678c5899fc2a9
no-intel <tkdduf212@gmail.com>
2023-11-06 13:12:30 +0900

    유기동물 상세 페이지

---
+++ src/main/java/com/noint/shelterzoo/domain/abandoned/vo/res/AbandonedDetailResponseVO.java
@@ -0,0 +1,23 @@
+package com.noint.shelterzoo.domain.abandoned.vo.res;
+
+import lombok.Data;
+
+@Data
+public class AbandonedDetailResponseVO {
+    private long seq;
+    private String happenPlace;
+    private String kind;
+    private String kindDetail;
+    private String color;
+    private String birth;
+    private String weight;
+    private String noticeEnd;
+    private String gender;
+    private String neuter;
+    private String specialMark;
+    private String img;
+    private String adoptProcess;
+    private String shelterName;
+    private String shelterTel;
+    private String shelterAddr;
+}
