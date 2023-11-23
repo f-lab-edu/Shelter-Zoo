322dfc9f3ec053717265e8fc4046017d5c161f3f
tkdduf212@gmail.com <tkdduf212@gmail.com>
2023-11-24 04:54:48 +0900

    서버정보 확인 컨트롤러 추가

--- src/main/java/com/noint/shelterzoo/ServerInfoController.java
+++ src/main/java/com/noint/shelterzoo/ServerInfoController.java
@@ -7,14 +7,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class VersionCheckController {
+public class ServerInfoController {
     @Value("${server.port}")
-    private Integer port;
+    private String port;
     @Value("${version}")
     private String version;
 
     @GetMapping("/version")
-    public ResponseEntity<String> version(){
-        return new ResponseEntity<>("port : " + port + ", version : " + version, HttpStatus.OK);
+    public ResponseEntity<String> version() {
+        return new ResponseEntity<>(version, HttpStatus.OK);
+    }
+
+    @GetMapping("/port")
+    public ResponseEntity<String> port() {
+        return new ResponseEntity<>(port, HttpStatus.OK);
     }
 }
