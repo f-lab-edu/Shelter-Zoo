98cb84315e6766516483454433c78dc8d1f33230
tkdduf212@gmail.com <tkdduf212@gmail.com>
2024-01-07 15:36:34 +0900

    db 커넥션 풀 100개 적용

--- src/main/java/com/noint/shelterzoo/config/DBConfigByOne.java
+++ src/main/java/com/noint/shelterzoo/config/DBConfigByOne.java
@@ -1,5 +1,7 @@
 package com.noint.shelterzoo.config;
 
+import com.zaxxer.hikari.HikariDataSource;
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
@@ -8,19 +10,21 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import javax.sql.DataSource;
 import java.io.IOException;
 
+@Slf4j
 @Configuration
 @EnableTransactionManagement
 @MapperScan(basePackages = "com.noint.shelterzoo.domain.*.repository")
-@Profile("{local, dev1DB}")
+@Profile("dev1DB")
 public class DBConfigByOne {
     @Bean
-    public SqlSessionFactoryBean sqlSessionFactoryBean(DataSource dataSource) throws IOException {
+    public SqlSessionFactoryBean sqlSessionFactoryBean(HikariDataSource dataSource) throws IOException {
         SqlSessionFactoryBean sessionFactoryBean = new SqlSessionFactoryBean();
         sessionFactoryBean.setDataSource(dataSource);
         sessionFactoryBean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath:mapper/domain/**/*.xml"));
+        log.info("Max-Pool-Size : {}", dataSource.getMaximumPoolSize());
+        log.info("Min-Idle : {}", dataSource.getMinimumIdle());
         return sessionFactoryBean;
     }
 }
